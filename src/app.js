7bfd703190b539a85071ca61774253219adc97ac
andreamazza84 <andrea.mazza84@gmail.com>
2021-01-26 19:00:58 +0100

    lavoro su main

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 let app = new Vue({
     el: '#root',
     data: {
-        imgpath_1: 'dist/img/parte-1',
-        imgpath_2: 'dist/img/parte-2',
-        imgpath_3: 'dist/img/parte-3',
+        imgcsspath: '../',
+        imgpath_1: 'dist/img/parte-1/',
+        imgpath_2: 'dist/img/parte-2/',
+        imgpath_3: 'dist/img/parte-3/',
+        img:{
+            sitelogo: 'logo.svg',
+            ill_1:'illustration-main.jpg',
+            google: 'google-logo.svg',
+            slack: 'slack-logo.svg',
+        },
     },
     computed: {},
     methods: {},
