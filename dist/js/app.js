d3783817a71da159f650933f719408080c5388dc
andreamazza84 <andrea.mazza84@gmail.com>
2021-01-26 17:24:09 +0100

    #navbar

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,7 +7,18 @@
   \********************/
 /***/ (() => {
 
-
+var app = new Vue({
+  el: '#root',
+  data: {
+    imgpath_1: 'dist/img/parte-1',
+    imgpath_2: 'dist/img/parte-2',
+    imgpath_3: 'dist/img/parte-3'
+  },
+  computed: {},
+  methods: {},
+  created: function created() {},
+  mounted: function mounted() {}
+});
 
 /***/ }),
 
--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 let app = new Vue({
     el: '#root',
-    data: {},
+    data: {
+        imgpath_1: 'dist/img/parte-1',
+        imgpath_2: 'dist/img/parte-2',
+        imgpath_3: 'dist/img/parte-3',
+    },
     computed: {},
     methods: {},
     created(){},
