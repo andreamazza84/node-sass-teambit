cc0582238e2be7d3c93b63359f1a36da6201fcf1
andreamazza84 <andrea.mazza84@gmail.com>
2021-01-27 12:58:20 +0100

    #footer quasi finito

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ let app = new Vue({
         imgpath: 'dist/img/',
         img:{
             sitelogo: 'logo.svg',
+            small_logo: 'logo-small.svg',
             ill_1:'illustration-main.jpg',
             google: 'google-logo.svg',
             slack: 'slack-logo.svg',
@@ -31,6 +32,42 @@ let app = new Vue({
         },
         chat:{
             value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, consequuntur laborum inventore soluta qui quae eum accusantium autem culpa necessitatibus aspernatur? Ex incidunt voluptates quas officia nemo dolor odit dicta suscipit vitae ad in amet aperiam culpa reiciendis veritatis dignissimos corporis eligendi perferendis cum, saepe earum temporibus consequatur alias sed? Quibusdam enim suscipit error voluptatibus esse eos quo molestiae quos, dolorem repudiandae doloribus numquam. Quam voluptatum voluptates, dicta ad, tempora rem at odio, consequuntur aperiam animi obcaecati. Doloribus quam vero quod natus sequi, perspiciatis impedit expedita incidunt culpa ad, sint ipsum optio numquam dolorum harum voluptate cumque vel nisi architecto placeat! Quam assumenda architecto distinctio minima repudiandae voluptatibus nostrum. Quae, fuga ratione. Porro ullam voluptate a ex iusto velit repudiandae quasi, vero eum ipsam maxime? Quasi optio natus ducimus autem quas ea ipsam tenetur, neque adipisci unde assumenda harum quis enim earum quod laboriosam provident voluptate cum? Impedit asperiores repellendus unde omnis dignissimos, ullam eum consequatur similique minima reiciendis officiis sed? Quam eveniet doloremque qui molestiae expedita. Rem molestias tempore odio doloribus perspiciatis excepturi, modi dicta rerum architecto repellat, harum voluptatem commodi. Esse numquam odit, ex nemo, veniam dignissimos quod dolorem nihil iste molestiae in placeat libero excepturi ea minima.'
+        },
+        menu:{
+            product: [
+                'feedback',
+                'surveys',
+                'reviews',
+                'praise',
+                'slack',
+                'integration',
+                'schedule a demo',
+                'pricing'
+            ], 
+            about:[
+                'feedback',
+                'surveys',
+                'reviews',
+                'praise'
+            ],
+            connect:[
+                'feedback',
+                'surveys',
+                'reviews',
+                'praise'
+            ],
+            lobs:[
+                'feedback',
+                'surveys',
+                'reviews',
+                'praise'
+            ],
+            fromtheblog:[
+                'how to change your company culture: a four-step framework',
+                'how teams can be more productive by killing 8-hour workday',
+                'how to build high performing teams inspired by nature',
+                'view all'
+            ],
         },
     },
     computed: {},
