49dc0161c475cbfd82ec08851279c62226c20558
andreamazza84 <andrea.mazza84@gmail.com>
2021-01-27 11:48:01 +0100

    #footer

--- dist/js/app.js
+++ dist/js/app.js
@@ -32,14 +32,21 @@ var app = new Vue({
       em_review: 'performance-review.jpg',
       review: 'review.jpg',
       em_survey: 'employee-survey.jpg',
-      recognition: 'recognition.jpg'
+      recognition: 'recognition.jpg',
+      chat: 'chat.svg',
+      happy: 'happy.svg',
+      face_1: 'alex.png',
+      face_2: 'bohdan.png'
+    },
+    chat: {
+      value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, consequuntur laborum inventore soluta qui quae eum accusantium autem culpa necessitatibus aspernatur? Ex incidunt voluptates quas officia nemo dolor odit dicta suscipit vitae ad in amet aperiam culpa reiciendis veritatis dignissimos corporis eligendi perferendis cum, saepe earum temporibus consequatur alias sed? Quibusdam enim suscipit error voluptatibus esse eos quo molestiae quos, dolorem repudiandae doloribus numquam. Quam voluptatum voluptates, dicta ad, tempora rem at odio, consequuntur aperiam animi obcaecati. Doloribus quam vero quod natus sequi, perspiciatis impedit expedita incidunt culpa ad, sint ipsum optio numquam dolorum harum voluptate cumque vel nisi architecto placeat! Quam assumenda architecto distinctio minima repudiandae voluptatibus nostrum. Quae, fuga ratione. Porro ullam voluptate a ex iusto velit repudiandae quasi, vero eum ipsam maxime? Quasi optio natus ducimus autem quas ea ipsam tenetur, neque adipisci unde assumenda harum quis enim earum quod laboriosam provident voluptate cum? Impedit asperiores repellendus unde omnis dignissimos, ullam eum consequatur similique minima reiciendis officiis sed? Quam eveniet doloremque qui molestiae expedita. Rem molestias tempore odio doloribus perspiciatis excepturi, modi dicta rerum architecto repellat, harum voluptatem commodi. Esse numquam odit, ex nemo, veniam dignissimos quod dolorem nihil iste molestiae in placeat libero excepturi ea minima.'
     }
   },
   computed: {},
   methods: {},
   created: function created() {},
   mounted: function mounted() {}
-});
+}); //:src="imgpath+img.chat.svg"
 
 /***/ }),
 
